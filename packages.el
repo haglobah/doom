b4535ea73f90a3bca2de0a10bfbdaa462613811f
Beat Hagenlocher <bah@posteo.de>
2025-01-09 16:38:45 +0100

    Add catppuccin theme

--- packages.el
+++ packages.el
@@ -21,6 +21,8 @@
 (package! affe
   :recipe (:host github :repo "minad/affe"))
 
+(package! catppuccin-theme)
+
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
