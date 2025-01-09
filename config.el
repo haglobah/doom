11cc0d856829d0df71043e2f16c54c145d107675
Beat Hagenlocher <bah@posteo.de>
2025-01-09 14:37:09 +0100

    Add better window management

--- config.el
+++ config.el
@@ -63,6 +63,20 @@
 
 (map! :nv "M-." (lambda () (interactive) (affe-find "~")))
 
+(map! :map evil-window-map
+      "SPC" #'rotate-layout
+
+      "<left>"  #'evil-window-left
+      "<down>"  #'evil-window-down
+      "<up>"    #'evil-window-up
+      "<right>" #'evil-window-right
+
+      "C-<left>"       #'+evil/window-move-left
+      "C-<down>"       #'+evil/window-move-down
+      "C-<up>"         #'+evil/window-move-up
+      "C-<right>"      #'+evil/window-move-right)
+
+
 (after! why-this
   (setq! why-this-idle-delay 0.01)
   (global-why-this-mode t))
