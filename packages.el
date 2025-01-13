bc4a09a21f942099e1f84254f7ed17b1b8136fd8
Beat Hagenlocher <bah@posteo.de>
2025-01-13 14:17:26 +0100

    Add all-the-icons

--- packages.el
+++ packages.el
@@ -25,6 +25,8 @@
 
 (package! catppuccin-theme)
 
+(package! all-the-icons)
+
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
