953442d98e7d5172c6240d05f3e51128b13aead6
Beat Hagenlocher <bah@posteo.de>
2025-05-14 11:27:55 +0200

    Make buffer visit file

--- config/keybindings/workspaces.el
+++ config/keybindings/workspaces.el
@@ -1,17 +1,14 @@
 ;;; config/keybindings/workspaces.el -*- lexical-binding: t; -*-
 
-
 (defun bah/setup-workspaces ()
   (interactive)
 
-  (dolist (name+dir '(("mycelium" "~/mycelium/")
-                      ("doom" "~/.config/doom/")
-                      ("nix-home" "~/nix-home/")
-                      ))
-    (persp-add-new (car name+dir))
-    m
-    m
-    (persp-switch (car name+dir))
-    (dired (cdr name+dir))))
+  (dolist (name+dir+file '(("mycelium" "~/mycelium/" "Projects.md")
+                           ("doom" "~/.config/doom/" "config.el")
+                           ("nix-home" "~/nix-home/" "home.nix")
+                           ))
+    (persp-add-new (car name+dir+file))
+    (persp-switch (car name+dir+file))
+    (find-file (concat (cadr name+dir+file) (caddr name+dir+file)))))
 
 (map! :leader "TAB w" #'bah/setup-workspaces)
