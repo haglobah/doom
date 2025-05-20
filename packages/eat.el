55fff1a6c6569824e75cdeff95e73c0148a52332
Beat Hagenlocher <bah@posteo.de>
2025-05-20 13:27:47 +0200

    Add eat keybindings

--- packages/eat.el
+++ packages/eat.el
@@ -1,3 +1,8 @@
 ;;; packages/eat.el -*- lexical-binding: t; -*-
 
-(setq eat-shell "/home/beat/.nix-profile/bin/fish")
+(setq eat-shell (executable-find "fish")
+      eat-kill-buffer-on-exit t)
+
+(map! :leader
+      :desc "Toggle other terminal window" "o t" #'eat-project-other-window
+      :desc "Toggle terminal window" "o T" #'eat-project)
