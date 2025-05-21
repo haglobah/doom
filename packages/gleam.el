8902311539035e5e7e0de9e4fb06655d1bfd719d
Beat Hagenlocher <bah@posteo.de>
2025-05-21 13:57:10 +0200

    Fix gleam mode

--- packages/gleam.el
+++ packages/gleam.el
@@ -2,9 +2,17 @@
 (use-package! gleam-ts-mode
   :mode (rx ".gleam" eos))
 
-(after! treesit
-  (add-to-list 'auto-mode-alist '("\\.gleam$" . gleam-ts-mode)))
+(add-to-list 'auto-mode-alist '("\\.gleam$" . gleam-ts-mode))
+
+(setq lsp-gleam-executable '("gleam" "lsp"))
 
 (after! gleam-ts-mode
   (unless (treesit-language-available-p 'gleam)
     (gleam-ts-install-grammar)))
+
+(map! :map gleam-ts-mode-map
+      :localleader
+      :desc "Format" :nv "c f" #'gleam-ts-format
+      :desc "Indent sexp" :nv "c i" #'prog-indent-sexp
+      :desc "Start defun" :nv "g e" #'treesit-end-of-defun
+      :desc "End defun" :nv "g a" #'treesit-beginning-of-defun)
