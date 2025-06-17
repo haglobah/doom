6a29b293e1889408f655db20c336f2a3c8cc660a
Beat Hagenlocher <bah@posteo.de>
2025-06-17 11:42:11 +0200

    Make workspaces auto-open

--- config/keybindings/workspaces.el
+++ config/keybindings/workspaces.el
@@ -1,41 +1,62 @@
 ;;; config/keybindings/workspaces.el -*- lexical-binding: t; -*-
 
-(defvar bah/workspaces '(("doom" "~/.config/doom/" "config.el")
-                         ("nix-home" "~/nix-home/" "home.nix")
-                         ("mynix" "~/mynix/" "configuration.nix")
-                         ("beathagenlocher.com" "~/beathagenlocher.com/" "flake.nix")
-                         ("mycelium" "~/mycelium/" "acc.md")
-                         ("templater" "~/projects/templater/" "templates/flake.nix")
-                         ("zmk-config-tzcl" "~/projects/zmk-config-tzcl/" "config/rae_dux.keymap")
+(defvar bah/persistent-workspaces
+  '(("doom" "~/.config/doom/" "config.el")
+    ("nix-home" "~/nix-home/" "home.nix")
+    ("mynix" "~/mynix/" "configuration.nix")
+    ("beathagenlocher.com" "~/beathagenlocher.com/" "flake.nix")
+    ("mycelium" "~/mycelium/" "acc.md")
+    ("zmk-config-tzcl" "~/projects/zmk-config-tzcl/" "config/rae_dux.keymap")
+    ("templater" "~/projects/templater/" "templates/flake.nix")))
 
-                         ("fabresearcher" "~/ag/fabresearcher/work/" "Justfile")
-                         ("queue" "~/projects/rescript_instant_todo/" "flake.nix")
-                         ))
+(defvar bah/all-workspaces
+  (append
+   bah/persistent-workspaces
+   '(("fabresearcher" "~/ag/fabresearcher/work/" "Justfile")
+     ("queue" "~/projects/rescript_instant_todo/" "flake.nix"))))
+
+(cl-defun bah/get-workspace (name workspaces)
+  (interactive)
+  (dolist (name+dir+file workspaces)
+    (if (string-equal name (first name+dir+file))
+        (cl-return-from bah/get-workspace name+dir+file)
+      nil)))
+
+(defun bah/setup-workspace (name+dir+file)
+  (interactive)
+  (persp-add-new (first name+dir+file))
+  (persp-switch (first name+dir+file))
+  (find-file (concat (second name+dir+file) (caddr name+dir+file)))
+  (find-file (concat (second name+dir+file) (second (projectile-recentf-files)))))
 
 (defun bah/setup-workspaces (workspaces)
   (interactive)
-
   (dolist (name+dir+file workspaces)
-    (persp-add-new (first name+dir+file))
-    (persp-switch (first name+dir+file))
-    (find-file (concat (second name+dir+file) (caddr name+dir+file)))
-    (find-file (concat (second name+dir+file) (second (projectile-recentf-files))))))
+    (bah/setup-workspace name+dir+file)))
+
+(defun bah/create|switch (workspace-name)
+  (interactive)
+  (if (+workspace-exists-p workspace-name)
+      (+workspace-switch workspace-name)
+    (bah/setup-workspace (bah/get-workspace workspace-name bah/all-workspaces))))
 
 (map! :leader
-      :desc "Setup Workspaces"    "TAB w" (cmd! (bah/setup-workspaces bah/workspaces))
+      :desc "Setup Workspaces"    "TAB w" (cmd! (bah/setup-workspaces bah/persistent-workspaces))
       :desc "Delete Workspace"    "TAB x" #'+workspace/kill
       :desc "New Workspace"       "TAB N" #'+workspace/new
 
-      :desc "Launch kitty tab"    "TAB ," (cmd! (shell-command (concat "kitty @ --to=unix:/tmp/mykitty launch --type=tab --cwd=" (projectile-project-root (file-name-directory (buffer-file-name))) " fish")))
+      :desc "Launch kitty tab"    "TAB ," (cmd! (shell-command (concat "kitty @ --to=unix:/tmp/mykitty launch --type=tab --cwd="
+                                                                       (projectile-project-root (file-name-directory (buffer-file-name)))
+                                                                       " fish")))
 
-      :desc "doom"                "TAB d" (cmd! (+workspace-switch "doom"))
-      :desc "nix-home"            "TAB n" (cmd! (+workspace-switch "nix-home"))
-      :desc "mynix"               "TAB m" (cmd! (+workspace-switch "mynix"))
-      :desc "beathagenlocher.com" "TAB b" (cmd! (+workspace-switch "beathagenlocher.com"))
-      :desc "mycelium"            "TAB o" (cmd! (+workspace-switch "mycelium"))
-      :desc "templater"           "TAB t" (cmd! (+workspace-switch "templater"))
-      :desc "zmk-config-tzcl"     "TAB z" (cmd! (+workspace-switch "zmk-config-tzcl"))
+      :desc "doom"                "TAB d" (cmd! (bah/create|switch "doom"))
+      :desc "nix-home"            "TAB n" (cmd! (bah/create|switch "nix-home"))
+      :desc "mynix"               "TAB m" (cmd! (bah/create|switch "mynix"))
+      :desc "beathagenlocher.com" "TAB b" (cmd! (bah/create|switch "beathagenlocher.com"))
+      :desc "mycelium"            "TAB o" (cmd! (bah/create|switch "mycelium"))
+      :desc "templater"           "TAB t" (cmd! (bah/create|switch "templater"))
+      :desc "zmk-config-tzcl"     "TAB z" (cmd! (bah/create|switch "zmk-config-tzcl"))
 
-      :desc "fabresearcher"       "TAB f" (cmd! (+workspace-switch "fabresearcher"))
-      :desc "queue"               "TAB q" (cmd! (+workspace-switch "queue"))
+      :desc "fabresearcher"       "TAB f" (cmd! (bah/create|switch "fabresearcher"))
+      :desc "queue"               "TAB q" (cmd! (bah/create|switch "queue"))
       )
