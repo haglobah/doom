c4004efa3c1f9cac71252dead8f2b2765142e290
Beat Hagenlocher <bah@posteo.de>
2025-06-27 14:33:23 +0200

    Add magit-refresh with "g r" in status buffer

--- config/magit.el
+++ config/magit.el
@@ -1,4 +1,7 @@
 ;;; magit.el ---                                     -*- lexical-binding: t; -*-
 
+(map! :map magit-status-mode-map
+      :nv "g r" #'magit-refresh)
+
 (after! magit
   (setq magit-diff-refine-hunk 'all))
