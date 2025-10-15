96c673707421f3df6a226a924ecf3bfb4cae6fe8
Beat Hagenlocher <bah@posteo.de>
2025-10-15 23:12:48 +0200

    Add aidermacs file options

--- packages/aidermacs.el
+++ packages/aidermacs.el
@@ -2,6 +2,8 @@
 
 (use-package! aidermacs
   :config
+  (setq! aidermacs-global-read-only-files '("~/mycelium/3_Resources/Coding/CONVENTIONS.md"))
+  (setq! aidermacs-project-read-only-files '("CONVENTIONS.md"))
   ;; (setq! aider-args '("--model" "gpt-4o-mini"))
   (map! :g "C-c a" #'aidermacs-transient-menu)
   :custom
