788146a0d4a1ff913c3c26bd0cf1a347a58a1dad
Beat Hagenlocher <bah@posteo.de>
2025-10-28 00:19:53 +0100

    Choose right aider version

--- packages/aidermacs.el
+++ packages/aidermacs.el
@@ -4,6 +4,7 @@
   :config
   (setq! aidermacs-global-read-only-files '("~/mycelium/3_Resources/Coding/CONVENTIONS.md"))
   (setq! aidermacs-project-read-only-files '("CONVENTIONS.md"))
+  (setq! aidermacs-program "aider")
   ;; (setq! aider-args '("--model" "gpt-4o-mini"))
   (map! :g "C-c a" #'aidermacs-transient-menu)
   :custom
