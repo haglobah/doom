6fd56c0f254b7e257a758f0d89e75f0f8f26502c
Beat Hagenlocher <bah@posteo.de>
2025-11-15 19:09:05 +0100

    Publish works

--- packages/bluesky.el
+++ packages/bluesky.el
@@ -1,29 +1,33 @@
 ;;; -*- lexical-binding: t; -*-
 
 (load! "../config/utils.el")
+(load! "bluesky/mdx-to-richtext.el")
 
 (defcustom bluesky-garden-base-url "https://beathagenlocher.com/"
   "Base URL for your digital garden"
   :type 'string
   :group 'bluesky)
 
-(defcustom bsky-post-url "https://localhost:3000/post"
-  "URL for your local bsky-post server"
-  :type 'string
-  :group 'bsky)
-
-(defun bsky-post (text)
+(defun bsky-post (text.facets)
   "Post current buffer content to Bluesky."
   (interactive)
   (let* ((url-request-method "POST")
          (url-request-extra-headers '(("Content-Type" . "application/json")))
-         (url-request-data (json-encode `((text . ,text)))))
+         (url-request-data (json-encode `((text . ,(car text.facets))
+                                          (facets . ,(cdr text.facets))))))
     (url-retrieve
-     url
+     "http://localhost:3000/post"
      (lambda (status)
        (if (plist-get status :error)
            (message "Bsky post failed: %s" (plist-get status :error))
          (message "Posted to Bluesky"))))))
+
+[
+ (->> "Is this finally a [nonbroken publish](https://github.com/haglobah/beathagenlocher.com/blob/53c04162ee0cd1ef12145b5b137055ffd9af921e/bsky-post-server/index.ts#L1) from [[Emacs]] by @beathagenlocher.com?"
+      (bluesky--parse-mdx-to-richtext)
+      (bsky-post))
+ ]
+
 
 (defun bsky-post-region (start end)
   (interactive "r")
