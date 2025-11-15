78fac173ae9ccc69d8ae8766f5bf95c652bc1005
Beat Hagenlocher <bah@posteo.de>
2025-11-15 17:52:53 +0100

    Refactor bsky to use new server

--- packages/bluesky.el
+++ packages/bluesky.el
@@ -1,112 +1,48 @@
-;;; config/keybindings/bluesky.el -*- lexical-binding: t; -*-
+;;; -*- lexical-binding: t; -*-
 
-(defcustom bluesky-handle ""
-  "Your Bluesky handle (username.bsky.social)"
-  :type 'string
-  :group 'bluesky)
-
-(defcustom bluesky-app-password ""
-  "Your Bluesky app password"
-  :type 'string
-  :group 'bluesky)
+(load! "../config/utils.el")
 
 (defcustom bluesky-garden-base-url "https://beathagenlocher.com/"
   "Base URL for your digital garden"
   :type 'string
   :group 'bluesky)
 
-(defvar bluesky--session nil
-  "Current Bluesky session data")
+(defcustom bsky-post-url "https://localhost:3000/post"
+  "URL for your local bsky-post server"
+  :type 'string
+  :group 'bsky)
 
-(load! "./bluesky/mdx-to-richtext.el")
-
-(defun encode-text (record-data)
-  (json-encode `((collection . "app.bsky.feed.post")
-                 (record . ,record-data))))
-
-(defun bluesky-authenticate ()
-  "Authenticate with Bluesky and store session"
+(defun bsky-post (text)
+  "Post current buffer content to Bluesky."
   (interactive)
-  (request "https://bsky.social/xrpc/com.atproto.server.createSession"
-    :type "POST"
-    :headers '(("Content-Type" . "application/json"))
-    :data (json-encode `((identifier . ,bluesky-handle)
-                         (password . ,bluesky-app-password)))
-    :parser 'json-read
-    :success (cl-function
-              (lambda (&key data &allow-other-keys)
-                (setq bluesky--session data)
-                (message "Bluesky authentication successful")))
-    :error (cl-function
-            (lambda (&rest args &key error-thrown &allow-other-keys)
-              (message "Bluesky auth failed: %s" error-thrown)))))
+  (let* ((url-request-method "POST")
+         (url-request-extra-headers '(("Content-Type" . "application/json")))
+         (url-request-data (json-encode `((text . ,text)))))
+    (url-retrieve
+     url
+     (lambda (status)
+       (if (plist-get status :error)
+           (message "Bsky post failed: %s" (plist-get status :error))
+         (message "Posted to Bluesky"))))))
 
-(defun bluesky-post-richtext (text.facets)
-  "Post TEXT with FACETS to Bluesky as rich text"
-  (let* ((text (car text.facets))
-         (facets (cdr text.facets)))
-    (unless bluesky--session
-      (bluesky-authenticate))
+(defun bsky-post-region (start end)
+  (interactive "r")
+  (let ((text (buffer-substring-no-properties start end)))
+    (bsky-post text)))
 
-    (when bluesky--session
-      (let ((access-token (alist-get 'accessJwt bluesky--session))
-            (did (alist-get 'did bluesky--session))
-            (record-data `((text . ,text)
-                           (createdAt . ,(format-time-string "%Y-%m-%dT%H:%M:%S.%3NZ" (current-time) t)))))
-
-        ;; Add facets if they exist
-        (when facets
-          (setq record-data (append record-data `((facets . ,(vconcat facets))))))
-
-        (request "https://bsky.social/xrpc/com.atproto.repo.createRecord"
-          :type "POST"
-          :headers `(("Authorization" . ,(format "Bearer %s" access-token))
-                     ("Content-Type" . "application/json"))
-          :data (json-encode `((repo . ,did)
-                               (collection . "app.bsky.feed.post")
-                               (record . ,record-data)))
-          :parser 'json-read
-          :success (cl-function
-                    (lambda (&key data &allow-other-keys)
-                      (message "Posted to Bluesky successfully with rich text")))
-          :error (cl-function
-                  (lambda (&rest args &key error-thrown &allow-other-keys)
-                    (message "Bluesky post failed: %s" error-thrown))))))))
+(defun bsky-post-buffer ()
+  (interactive)
+  (let ((text (buffer-substring-no-properties (point-min) (point-max))))
+    (bsky-post text)))
 
 [
- (-> "[[Learning to C]]"
-     (mdx->text.facets)
-     (bluesky-post-richtext))
+ (->> "[[Learning to C]]"
+      (bah/i)
+      (bsky-post))
  ]
-
-(defun mdx->text.facets (mdx)
-  (interactive)
-  (let* ((content mdx)
-         (parsed (bluesky--parse-mdx-to-richtext content)))
-    parsed))
-
-(defun inspect (thing)
-  (message thing)
-  thing)
-
-(defun mdx-buffer->text.facets ()
-  (interactive)
-  (inspect (mdx->text.facets (buffer-substring-no-properties (point-min) (point-max)))))
-
-(defun mdx-region->text.facets (start end)
-  (interactive "r")
-  (mdx->text.facets (buffer-substring-no-properties start end)))
-
-(defun publish-content ()
-  (let* ((text.facets (mdx-buffer->text.facets)))
-    (bluesky-post-richtext text.facets)))
-
-(setq bluesky-handle "beathagenlocher.com")
-(setq bluesky-app-password (getenv "BLUESKY_APP_SECRET"))
 
 (map! :map markdown-mode-map
       :leader
       :prefix ("d b" . "dg bsky")
-      :desc "Authenticate" "a" #'bluesky-authenticate
       :desc "Publish" "p" #'publish-content
       )
