3261f53e9b82da9cd11a09a09103171682bfed83
Beat Hagenlocher <bah@posteo.de>
2025-11-19 12:51:57 +0100

    Make bsky.el work

--- packages/bluesky.el
+++ packages/bluesky.el
@@ -13,14 +13,16 @@
   (interactive)
   (let* ((url-request-method "POST")
          (url-request-extra-headers '(("Content-Type" . "application/json")))
-         (url-request-data (json-encode `((text . ,(car text.facets))
-                                          (facets . ,(cdr text.facets))))))
+         (url-request-data (encode-coding-string
+                            (json-encode `((text . ,(car text.facets))
+                                           (facets . ,(cdr text.facets))))
+                            'utf-8)))
     (url-retrieve
      "http://localhost:3000/post"
      (lambda (status)
        (if (plist-get status :error)
            (message "Bsky post failed: %s" (plist-get status :error))
-         (message "Posted to Bluesky"))))))
+         (message "Posted to Bluesky: %s" status))))))
 
 [
  (->> "Is this finally a [nonbroken publish](https://github.com/haglobah/beathagenlocher.com/blob/53c04162ee0cd1ef12145b5b137055ffd9af921e/bsky-post-server/index.ts#L1) from [[Emacs]] by @beathagenlocher.com?"
@@ -28,16 +30,26 @@
       (bsky-post))
  ]
 
+(defun bsky-append-posse-backlink (text)
+  "Adds the POSSE backlink caption"
+  (concat text "
+
+[Syndicated](https://indieweb.org/POSSE) from my [digital garden](https://beathagenlocher.com)"))
 
 (defun bsky-post-region (start end)
   (interactive "r")
   (let ((text (buffer-substring-no-properties start end)))
-    (bsky-post text)))
+    (->> text
+         (bluesky--parse-mdx-to-richtext)
+         (bsky-post))))
 
 (defun bsky-post-buffer ()
   (interactive)
   (let ((text (buffer-substring-no-properties (point-min) (point-max))))
-    (bsky-post text)))
+    (->> text
+         (bsky-append-posse-backlink)
+         (bluesky--parse-mdx-to-richtext)
+         (bsky-post))))
 
 [
  (->> "[[Learning to C]]"
