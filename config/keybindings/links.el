d9ae891cb25b5758eeedd9db6fc25ec0e1c46f3c
Beat Hagenlocher <bah@posteo.de>
2025-11-20 12:07:31 +0100

    Add private links

--- config/keybindings/links.el
+++ config/keybindings/links.el
@@ -1,18 +1,29 @@
 ;;; config/keybindings/links.el -*- lexical-binding: t; -*-
 
-(defvar bah/standard-links
+(defvar bah/priv-links
+  '("focus.nirvanahq.com"
+    "app.mochi.cards"))
+
+(defvar bah/work-links
   '("mattermost.active-group.de"
     "timetracking.active-group.de"
-    ;; "projects.active-group.de"
     "focus.nirvanahq.com"
-    "app.mochi.cards"))
+    "app.mochi.cards"
+    "https://projects.active-group.de/projects/fab-researcher/issues"))
+
+(defun bah/open-in-tabs (linklist)
+  (let* ((command (bah/i (concat "kitty @ --to=unix:@mykitty launch --type=tab firefox "
+                                 (mapconcat
+                                  (lambda (link) (concat "--new-tab \"" link))
+                                  linklist
+                                  "\" ")
+                                 "\""))))
+    command
+    (shell-command command)
+    ))
 
 (map! :leader
       :prefix ("e" . "bah")
-      :desc "Open: Work links" :nv "w"
-      (cmd! (shell-command
-             (concat "kitty @ --to=unix:@mykitty launch --type=tab firefox "
-                     (mapconcat
-                      (lambda (link) (concat "--new-tab " link))
-                      bah/standard-links
-                      " ")))))
+      :desc "Open: Work links" :nv "w" (cmd! (bah/open-in-tabs bah/work-links))
+      :desc "Open: Private links" :nv "p" (cmd! (bah/open-in-tabs bah/priv-links))
+      )
