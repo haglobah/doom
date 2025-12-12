34518d9c27a2e83aa24edbdc2a8632a2fe9b9cda
Beat Hagenlocher <bah@posteo.de>
2025-12-12 16:29:08 +0100

    Force tab thing to work

--- config/company.el
+++ config/company.el
@@ -2,4 +2,6 @@
 
 (after! company
   (keymap-unset company-active-map "<return>" t)
-  (define-key company-active-map (kbd "<tab>") #'company-complete-selection))
+  (keymap-unset company-active-map "RET" t)
+  (define-key company-active-map (kbd "<tab>") #'company-complete-selection)
+  (define-key company-active-map (kbd "TAB") #'company-complete-selection))
