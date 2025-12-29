7177721f6997779f15ad4fa82421f40050ff392b
Beat Hagenlocher <bah@posteo.de>
2025-12-29 11:11:51 +0100

    Add commit & amend all

--- config/magit.el
+++ config/magit.el
@@ -3,5 +3,11 @@
 (map! :map magit-status-mode-map
       :nv "g r" #'magit-refresh)
 
+(map! :leader
+      :desc "Commit all" :nv "g !" (cmd!
+                                    (magit-commit-create (list "--all" "--message" "Add")))
+      :desc "Amend all" :nv "g @" (cmd!
+                                    (magit-commit-create (list "--all" "--amend" "--message" "Add"))))
+
 (after! magit
   (setq magit-diff-refine-hunk 'all))
