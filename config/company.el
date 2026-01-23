ca2f33969d8789e30da83fca2cdefcd4f5e40817
Beat Hagenlocher <bah@posteo.de>
2026-01-23 17:58:16 +0100

    Add way smaller delays

--- config/company.el
+++ config/company.el
@@ -4,4 +4,7 @@
   (keymap-unset company-active-map "<return>" t)
   (keymap-unset company-active-map "RET" t)
   (define-key company-active-map (kbd "<tab>") #'company-complete-selection)
-  (define-key company-active-map (kbd "TAB") #'company-complete-selection))
+  (define-key company-active-map (kbd "TAB") #'company-complete-selection)
+  (setq company-idle-delay 0.1)
+  (setq company-tooltip-idle-delay 0.1)
+  (setq company-minimum-prefix-length 1))
