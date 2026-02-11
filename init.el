643e88f2ae0609d21cda5b73abf8bcc7f4941c8c
Beat Hagenlocher <bah@posteo.de>
2026-02-11 02:10:59 +0100

    Add hash-lang mode

--- init.el
+++ init.el
@@ -163,7 +163,7 @@
        ;;purescript        ; javascript, but functional
        (python +lsp)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
-       (racket +lsp +xp +lsp)            ; a DSL for DSLs
+       (racket +lsp +xp +hash-lang)            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
