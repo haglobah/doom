b73aafeb4f70cfce4eb9e7bac8f92cce7c61b966
Beat Hagenlocher <bah@posteo.de>
2026-02-23 10:33:26 +0100

    Do the ibus thing in kitty

--- config/keybindings.el
+++ config/keybindings.el
@@ -12,7 +12,7 @@
 ;; For that, an `ibus' restart is sufficient
 (map! :leader
       :prefix ("e" . "bah")
-      :desc "Restart ibus" :nv "i" (cmd! (shell-command "ibus restart &")))
+      :desc "Restart ibus in kitty" :nv "i" (cmd! (shell-command "kitty @ --to=unix:@mykitty launch --type=tab ibus restart &")))
 
 (defun delete-word (arg)
   "Delete characters forward until encountering the end of a word.
