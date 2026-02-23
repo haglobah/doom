c852e7a1e6e25a80b95bbdcbbe118ec75256f06b
Beat Hagenlocher <bah@posteo.de>
2026-02-23 10:26:58 +0100

    Add grafana

--- config/keybindings/links.el
+++ config/keybindings/links.el
@@ -3,7 +3,8 @@
 (defvar bah/priv-links
   '("focus.nirvanahq.com"
     "eu.posthog.com"
-    "https://linklonk.com/foryou/stats"))
+    "https://linklonk.com/foryou/stats"
+    "https://monitoring.hagenlocher.me/grafana/d/1e8e5fbf-7fc4-436c-85b8-245aa3f18f52/infrastructure-health?orgId=1&from=now-24h&to=now&timezone=browser&var-hostname=$__all&refresh=5s"))
 
 (defvar bah/work-links
   `("mattermost.active-group.de"
