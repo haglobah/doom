10540652a0c7b9b4abbfb0f51dc1910db225669e
Beat Hagenlocher <bah@posteo.de>
2026-04-29 10:52:45 +0200

    Adjust link

--- config/keybindings/links.el
+++ config/keybindings/links.el
@@ -3,7 +3,7 @@
 (defvar bah/priv-links
   '("focus.nirvanahq.com"
     "eu.posthog.com"
-    "https://linklonk.com/foryou/stats"
+    "https://foryou.club/stats"
     ;; "https://monitoring.hagenlocher.me/grafana/d/1e8e5fbf-7fc4-436c-85b8-245aa3f18f52/infrastructure-health?orgId=1&from=now-24h&to=now&timezone=browser&var-hostname=$__all&refresh=5s"
     ))
 
