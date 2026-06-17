55bdd543f3f8581493697976682fbed53716a6e2
Beat Hagenlocher <bah@posteo.de>
2026-06-17 15:58:52 +0200

    Adjust agent-shell to work for me

--- packages/agent-shell.el
+++ packages/agent-shell.el
@@ -1,5 +1,7 @@
 ;;; packages/agent-shell.el -*- lexical-binding: t; -*-
 
+(map! :leader "o s" #'agent-shell-send-region)
+
 (require 'acp)
 (require 'agent-shell)
 (use-package agent-shell
@@ -10,16 +12,26 @@
         :desc "Toggle agent-shell buffer" "o `" #'agent-shell-toggle
         :desc "Open agent-shell: Claude" "o c" #'agent-shell-anthropic-start-claude-code
         :desc "Open agent-shell: OpenAI" "o o" #'agent-shell-openai-start-codex)
+  :hook
+  (agent-shell-mode . (lambda () (whitespace-mode -1)))
+
   :config
-  (setq! agent-shell-anthropic-authentication
-         (agent-shell-anthropic-make-authentication
-          :api-key
-          (lambda () (getenv "ANTHROPIC_API_KEY"))))
-  (setq! agent-shell-openai-authentication
-         (agent-shell-openai-make-authentication
-          :api-key
-          (lambda () (getenv "OPENAI_API_KEY"))))
-  (setq! agent-shell-google-authentication
-         (agent-shell-google-make-authentication
-          :api-key
-          (lambda () (getenv "GOOGLE_API_KEY")))))
+  (setopt agent-shell-tool-use-expand-by-default t)
+  ;; (setopt agent-shell-thought-process-expand-by-default t)
+  (setopt agent-shell-user-message-expand-by-default t)
+  ;; (setopt agent-shell-highlight-blocks t)
+  ;; (setopt agent-shell-highlight-blocks t)
+  (setopt agent-shell-show-busy-indicator t)
+
+  (setopt agent-shell-anthropic-authentication
+          (agent-shell-anthropic-make-authentication
+           :api-key
+           (lambda () (getenv "ANTHROPIC_API_KEY"))))
+  (setopt agent-shell-openai-authentication
+          (agent-shell-openai-make-authentication
+           :api-key
+           (lambda () (getenv "OPENAI_API_KEY"))))
+  (setopt agent-shell-google-authentication
+          (agent-shell-google-make-authentication
+           :api-key
+           (lambda () (getenv "GOOGLE_API_KEY")))))
